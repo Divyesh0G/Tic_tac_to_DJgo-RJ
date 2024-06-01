418ed67fa3b0e17c0e6be940a9a736cc20467c89
Divyesh0G <dvtech94@gmail.com>
2024-06-01 20:17:12 +1000

    apilogin cereated

--- tic_tac_to/ttt_backend/base/urls.py
+++ tic_tac_to/ttt_backend/base/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path , include
-from .views import authView, home
+from django.urls import path , include, re_path
+from .views import *
+
 
 urlpatterns = [
         path("", home, name="home"),
-        path("signup/", authView, name='authView'),
+        path("accounts/signup/", authView, name='authView'),
         path("accounts/", include("django.contrib.auth.urls")),
+        re_path('login', login),
+        re_path('signup', signup),
+        re_path('test_token', test_token)
 ]
--- tic_tac_to/ttt_backend/ttt_backend/settings.py
+++ tic_tac_to/ttt_backend/ttt_backend/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
+    'base',
+    'rest_framework',
+    # 'test_apis'
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
